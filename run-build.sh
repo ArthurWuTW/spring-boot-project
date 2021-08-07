0296c50ced0a7e5be617c2ad655c5531c7dd4c36
ArthurWuTW <lion811004@gmail.com>
2021-08-07 00:42:44 +0000

    run-build

--- run-build.sh
+++ run-build.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 export HOST=user
-export TMP_DIR=/home/$HOST/Desktop/jenkins-tmp-dir
-export CONTAINER=project-java-tomcat
-
-cd $TMP_DIR
-git clone https://github.com/ArthurWuTW/spring-boot-project
-cd spring-boot-project
+export BUILD_DIR=build/libs
+export TOMCAT_WEBAPP_DIR=/home/user/tomcat/apache-tomcat-9.0.50/webapps
 
 ./gradlew clean
 ./gradlew build
+
+cp $BUILD_DIR/spring-boot-project-0.0.1-SNAPSHOT.war $TOMCAT_WEBAPP_DIR/demo.war
+
+
+git config --global user.email "lion811004@gmail.com"
+git config --global user.name "ArthurWuTW"
+
