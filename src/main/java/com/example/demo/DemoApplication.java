0e47707f85c231a3ff93b935b205c1e76ff10cb3
ArthurWuTW <lion811004@gmail.com>
2021-08-08 00:30:59 +0000

    important! disable tomcat auto configure for hbn

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	    DataSourceAutoConfiguration.class, 
+	    DataSourceTransactionManagerAutoConfiguration.class, 
+	    HibernateJpaAutoConfiguration.class
+	})
 @ComponentScan({"com.example.controller", "com.example.configuration"})
 public class DemoApplication {
 
